53a847bc89389b046c7a202dc0465e2a0ed8231e
brata <bratatkr@gmail.com>
2014-03-11 21:16:12 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/lab-fabio/';
+$config['base_url']	= 'http://labfabio.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
